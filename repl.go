a5a76f810d1ede4840458c2e2375089ffe483d0f
mperkinson <mperkinson@gmail.com>
2026-01-08 09:51:44 -0500

    added startRepl() function

--- repl.go
+++ repl.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func cleanInput(text string) []string {
-	lowerCaseString := strings.ToLower(text)
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
+
+func cleanInput(str string) []string {
+	lowerCaseString := strings.ToLower(str)
 	words := strings.Fields(lowerCaseString)
 	return words
 }
