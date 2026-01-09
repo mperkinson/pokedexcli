13de9fc13490be5fa2a16baaef1c2dd1043a36b2
mperkinson <mperkinson@gmail.com>
2026-01-09 13:23:37 -0500

    added logic for commands

--- repl.go
+++ repl.go
@@ -9,12 +9,29 @@ import (
 
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		text := scanner.Text()
 		cleanWords := cleanInput(text)
-		fmt.Printf("Your command was: %s\n", cleanWords[0])
+
+		if len(cleanWords) == 0 {
+			continue
+		}
+
+		command := cleanWords[0]
+
+		cmd, ok := commands[command]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
